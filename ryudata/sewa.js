63962c8c7470a3bdf24b5fec07feef0badd020a4
Zinstorm <zinstorm07@gmail.com>
2022-02-26 12:31:29 +0700

    v21

---
+++ ryudata/sewa.js
@@ -0,0 +1,23 @@
+exports.sewa = () => {
+	return`🇸 🇪 🇼 🇦  🇧 🇴 🇹 ❗
+
+┏━━━━━━
+┃𝗛𝗔𝗥𝗚𝗔 𝗦𝗘𝗪𝗔 :
+┃ *1 MINGGU : Rp 10.000*
+┃ *1 BULAN : Rp 30.000*
+┃ *PERMANEN : Rp 50.000*
+┗━━━━━━
+┏━━━━━━
+┃𝗙𝗜𝗧𝗨𝗥 𝗕𝗢𝗧 :
+┃ *⚔️ Antilink Group*
+┃ *👨‍🎨 Buat Stiker*
+┃ *📥 Downloader*
+┃ *Dll.*
+┗━━━━━━
+
+*SISTEM: TES BOT-MASUKIN BOT-TF-DONE*
+
+*MINAT? CHAT :
+wa.me/6288239099641
+`
+}
